9aae62a6da66dec2e3884536b4ff5edd63bb8764
I-L9614 <leviitamar034@gmail.com>
2025-12-24 15:47:59 +0200

    add addTask

--- controllers/tasksControllers.js
+++ controllers/tasksControllers.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { readTasks,writeTasks } from '../utils/readWrite.js'
+import { readTasks, writeTasks } from '../utils/readWrite.js'
 const tasksPath = './tasks.json'
 
 export async function getTasks(req, res) {
@@ -7,35 +7,48 @@ export async function getTasks(req, res) {
     res.json(tasks)
 }
 
-export async function getCompleted(req,res) {
+export async function getCompleted(req, res) {
     let tasks = await readTasks()
     const completed = req.query.completed
-    if (completed==='true') {
-        tasks = tasks.filter(task => task.completed===completed)
+    if (completed === 'true'||completed==='false') {
+        tasks = tasks.filter(task => task.completed === completed)
         if (tasks.length === 0) {
-            res.send("there is no completed tasks")
-        }
-        else{
-            res.json(tasks)
-        }
-    }
-    else if (completed === 'false') {
-        tasks = tasks.filter(task => task.completed===false)
-        if (tasks.length === 0) {
-            res.send("all tasks are completed")
+            res.send("nothing has found")
         }
         else {
             res.json(tasks)
         }
     }
-    else {
-        res.send('Task not found!')
+    else{
+        res.send('no such thing')
     }
 }
 
+export async function getTaskByPriority(req, res) {
+    let tasks = await readTasks()
+    const priority = req.query.priority
+    tasks = tasks.filter(task => task.priority===priority)
+    if (!priority) {
+        res.send("Wrong priority")
+    }
+    res.json(tasks)
+}
+
 export async function getTaskById(req, res) {
-  const tasks = await readTasks();
-  const result = tasks.find((task) => task.id === parseInt(req.params.id));
-  if (!result) res.status(404).send(`id - ${req.params.id} not found`);
-  res.json(result);
+    const tasks = await readTasks();
+    const result = tasks.find((task) => task.id === parseInt(req.params.id));
+    if (!result) res.status(404).send(`id - ${req.params.id} not found`);
+    res.json(result);
+}
+
+export async function addTask(req,res) {
+    const tasks = await readTasks();
+    const maxId = users.length > 0 ? Math.max(...users.map(u => u.id)) : 0
+    const newTask = {
+        id:maxId + 1,
+        ...req.body
+    }
+    tasks.push(newTask)
+    await writeTasks(tasks)
+    res.json(tasks)
 }
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import fs from 'fs/promises';
-import { readTasks,writeTasks } from './utils/readWrite.js';
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
-
-
 
 
 
